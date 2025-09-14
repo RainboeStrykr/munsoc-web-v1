3731ce28a9ded0dc1fbffc33acc3e9382a84273f
rainboestrykr <abhirajbhowmick27@gmail.com>
2025-09-14 18:44:12 +0530

    hid executive board

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -24,7 +24,7 @@ type HeaderLink = {
 const links: HeaderLink[] = [
   // { label: "Home", link: "/", showsAtHome: false },
   { label: "Our Legacy", link: "#manifesto", showsAtHome: true },
-  { label: "Executive Board", link: "/executive-board", showsAtHome: true },
+  // { label: "Executive Board", link: "/executive-board", showsAtHome: true },
   { label: "Committees", link: "/#tracks", showsAtHome: true },
   {
     label: "Country Matrix",
